599bf774f61383fce03231445727bb78a531791b
ryo <65832968+ryosafadi@users.noreply.github.com>
2024-11-04 11:04:35 -0800

    add test button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-// todo
+import "./style.css";
+
+const map = document.querySelector<HTMLDivElement>("#map")!;
+
+const starterButton = document.createElement("button");
+starterButton.innerHTML = "test";
+map.append(starterButton);
+
+starterButton.addEventListener("click", () => {
+  alert("button clicked!");
+});
