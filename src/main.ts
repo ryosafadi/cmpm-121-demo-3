99fddd993c0918c4ccb42fa2cf1399a20be23690
ryo <65832968+ryosafadi@users.noreply.github.com>
2024-11-05 12:19:00 -0800

    add world map

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,41 @@
+import leaflet from "leaflet";
+
+import "leaflet/dist/leaflet.css";
 import "./style.css";
 
-const map = document.querySelector<HTMLDivElement>("#map")!;
+import "./leafletWorkaround.ts";
+
+const APP_NAME = "Geocoin Carrier";
+
+document.title = APP_NAME;
+
+const controlPanel = document.querySelector<HTMLDivElement>("#controlPanel")!;
 
 const starterButton = document.createElement("button");
 starterButton.innerHTML = "test";
-map.append(starterButton);
+controlPanel.append(starterButton);
 
 starterButton.addEventListener("click", () => {
   alert("button clicked!");
 });
+
+const INITIAL_LOCATION = leaflet.latLng(36.98949379578401, -122.06277128548504);
+
+const GAMEPLAY_ZOOM_LEVEL = 19;
+
+const map = leaflet.map(document.getElementById("map")!, {
+  center: INITIAL_LOCATION,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
+});
+
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: GAMEPLAY_ZOOM_LEVEL,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
