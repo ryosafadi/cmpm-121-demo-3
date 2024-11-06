45b5252a2abccc9e3789290e6ce1e34681c5dc71
ryo <65832968+ryosafadi@users.noreply.github.com>
2024-11-05 20:52:06 -0800

    add player marker and caches

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import "./style.css";
 
 import "./leafletWorkaround.ts";
 
+import luck from "./luck.ts";
+
 const APP_NAME = "Geocoin Carrier";
 
 document.title = APP_NAME;
@@ -22,6 +24,9 @@ starterButton.addEventListener("click", () => {
 const INITIAL_LOCATION = leaflet.latLng(36.98949379578401, -122.06277128548504);
 
 const GAMEPLAY_ZOOM_LEVEL = 19;
+const TILE_DEGREES = 1e-4;
+const NEIGHBORHOOD_SIZE = 8;
+const CACHE_SPAWN_PROBABILITY = 0.1;
 
 const map = leaflet.map(document.getElementById("map")!, {
   center: INITIAL_LOCATION,
@@ -39,3 +44,66 @@ leaflet
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   })
   .addTo(map);
+
+const playerMarker = leaflet.marker(INITIAL_LOCATION);
+playerMarker.bindTooltip("Your Location");
+playerMarker.addTo(map);
+
+let playerPoints = 0;
+const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
+statusPanel.innerHTML = `${playerPoints} Points`;
+
+function spawnCache(i: number, j: number) {
+  const origin = INITIAL_LOCATION;
+  const bounds = leaflet.latLngBounds([
+    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
+    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
+  ]);
+
+  const rect = leaflet.rectangle(bounds, { color: "black" });
+  rect.addTo(map);
+
+  rect.bindPopup(() => {
+    let pointValue = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+
+    const popupDiv = document.createElement("div");
+    popupDiv.innerHTML = `
+                <div>There is a cache here at "${i},${j}". It has value <span id="value">${pointValue}</span>.</div>
+                <button id="collect">collect</button>
+                <button id="deposit">deposit</button>`;
+
+    popupDiv
+      .querySelector<HTMLButtonElement>("#collect")!
+      .addEventListener("click", () => {
+        if (pointValue > 0) {
+          pointValue--;
+          popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+            pointValue.toString();
+          playerPoints++;
+          statusPanel.innerHTML = `${playerPoints} Points`;
+        }
+      });
+
+    popupDiv
+      .querySelector<HTMLButtonElement>("#deposit")!
+      .addEventListener("click", () => {
+        if (playerPoints > 0) {
+          pointValue++;
+          popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML =
+            pointValue.toString();
+          playerPoints--;
+          statusPanel.innerHTML = `${playerPoints} Points`;
+        }
+      });
+
+    return popupDiv;
+  });
+}
+
+for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
+  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
+      spawnCache(i, j);
+    }
+  }
+}
