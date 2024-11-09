44ebe3845249a7860710829a78e396cc93757520
ryo <65832968+ryosafadi@users.noreply.github.com>
2024-11-08 17:42:13 -0800

    separate gameplay logic from UI (D3.a complete)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// @deno-types="npm:@types/leaflet@^1.9.14"
 import leaflet from "leaflet";
 
 import "leaflet/dist/leaflet.css";
@@ -8,79 +9,115 @@ import "./leafletWorkaround.ts";
 import luck from "./luck.ts";
 
 const APP_NAME = "Geocoin Carrier";
-
 document.title = APP_NAME;
 
-const controlPanel = document.querySelector<HTMLDivElement>("#controlPanel")!;
+interface Location {
+  latitude: number;
+  longitude: number;
+}
 
-const starterButton = document.createElement("button");
-starterButton.innerHTML = "test";
-controlPanel.append(starterButton);
+interface Cache {
+  points: number;
+}
 
-starterButton.addEventListener("click", () => {
-  alert("button clicked!");
+const OAKES_CLASSROOM = {
+  latitude: 36.98949379578401,
+  longitude: -122.06277128548504,
+};
+
+const bus = new EventTarget();
+
+let playerLocation: Location = OAKES_CLASSROOM;
+let playerPoints: number = 0;
+
+const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
+statusPanel.innerHTML = "0 Points";
+
+function collect(cache: Cache) {
+  playerPoints++;
+  cache.points--;
+  bus.dispatchEvent(new Event("points-changed"));
+}
+
+function deposit(cache: Cache) {
+  playerPoints--;
+  cache.points++;
+  bus.dispatchEvent(new Event("points-changed"));
+}
+
+bus.addEventListener("points-changed", () => {
+  statusPanel.innerHTML = `${playerPoints} Points`;
 });
 
-const INITIAL_LOCATION = leaflet.latLng(36.98949379578401, -122.06277128548504);
-
-const GAMEPLAY_ZOOM_LEVEL = 19;
-const TILE_DEGREES = 1e-4;
-const NEIGHBORHOOD_SIZE = 8;
-const CACHE_SPAWN_PROBABILITY = 0.1;
+function _movePlayer(destination: Location) {
+  playerLocation = destination;
+  bus.dispatchEvent(new Event("player-moved"));
+}
 
 const map = leaflet.map(document.getElementById("map")!, {
-  center: INITIAL_LOCATION,
-  zoom: GAMEPLAY_ZOOM_LEVEL,
-  minZoom: GAMEPLAY_ZOOM_LEVEL,
-  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  center: leaflet.latLng(playerLocation.latitude, playerLocation.longitude),
+  zoom: 19,
+  minZoom: 19,
+  maxZoom: 19,
   zoomControl: false,
   scrollWheelZoom: false,
 });
 
 leaflet
   .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: GAMEPLAY_ZOOM_LEVEL,
+    maxZoom: 19,
     attribution:
       '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   })
   .addTo(map);
 
-const playerMarker = leaflet.marker(INITIAL_LOCATION);
+const playerMarker = leaflet.marker(
+  leaflet.latLng(playerLocation.latitude, playerLocation.longitude),
+);
 playerMarker.bindTooltip("Your Location");
 playerMarker.addTo(map);
 
-let playerPoints = 0;
-const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
-statusPanel.innerHTML = `${playerPoints} Points`;
+bus.addEventListener("player-moved", () => {
+  playerMarker.setLatLng(
+    leaflet.latLng(playerLocation.latitude, playerLocation.longitude),
+  );
+});
+
+const TILE_DEGREES = 1e-4;
 
 function spawnCache(i: number, j: number) {
-  const origin = INITIAL_LOCATION;
+  const origin = playerLocation;
   const bounds = leaflet.latLngBounds([
-    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
-    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
+    [origin.latitude + i * TILE_DEGREES, origin.longitude + j * TILE_DEGREES],
+    [
+      origin.latitude + (i + 1) * TILE_DEGREES,
+      origin.longitude + (j + 1) * TILE_DEGREES,
+    ],
   ]);
 
-  const rect = leaflet.rectangle(bounds, { color: "black" });
-  rect.addTo(map);
+  const cacheZone = leaflet.rectangle(bounds, { color: "black" });
+  cacheZone.addTo(map);
 
-  rect.bindPopup(() => {
-    let pointValue = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+  const pointValue = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
 
+  const cache: Cache = {
+    points: pointValue,
+  };
+
+  cacheZone.bindPopup(() => {
     const popupDiv = document.createElement("div");
     popupDiv.innerHTML = `
-                <div>There is a cache here at "${i},${j}". It has value <span id="value">${pointValue}</span>.</div>
-                <button id="collect">collect</button>
-                <button id="deposit">deposit</button>`;
+      <div>There is a cache here at "${i},${j}". It has value <span id="value">${cache.points}</span>.</div>
+      <button id="collect">Collect</button>
+      <button id="deposit">Deposit</button>`;
 
     popupDiv
       .querySelector<HTMLButtonElement>("#collect")!
       .addEventListener("click", () => {
-        if (pointValue > 0) {
-          pointValue--;
-          popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-            pointValue.toString();
-          playerPoints++;
-          statusPanel.innerHTML = `${playerPoints} Points`;
+        if (cache.points > 0) {
+          collect(cache);
+          popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML = cache
+            .points.toString();
         }
       });
 
@@ -88,17 +125,18 @@ function spawnCache(i: number, j: number) {
       .querySelector<HTMLButtonElement>("#deposit")!
       .addEventListener("click", () => {
         if (playerPoints > 0) {
-          pointValue++;
-          popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-            pointValue.toString();
-          playerPoints--;
-          statusPanel.innerHTML = `${playerPoints} Points`;
+          deposit(cache);
+          popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML = cache
+            .points.toString();
         }
       });
 
     return popupDiv;
   });
 }
+
+const NEIGHBORHOOD_SIZE = 8;
+const CACHE_SPAWN_PROBABILITY = 0.1;
 
 for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
   for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
